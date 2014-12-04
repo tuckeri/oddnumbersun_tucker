edad31ac087d0f3933ad44ecdb691bb2147ec4fe
tuckeri <tuckeri@pagosa.k12.co.us>
2014-12-04 10:03:15 -0700

    my for next prodject
    stuff

---
+++ oddNumberSum_tucker/oddNumberSum_tucker/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim lngFactorial As Long = 0
+        Dim intNumber As Integer = Val(Me.txtNumber.Text)
+
+        Dim intC As Integer
+        For intC = 1 To intNumber
+            If intC Mod 2 <> 0 Then
+                lngFactorial = lngFactorial + intC
+            End If
+        Next intC
+
+        Me.lblResults.Text = "the sum of the odd numbers from 1 to " & intNumber & " is " & lngFactorial
+    End Sub
+
+    Private Sub txtNumber_TextChanged(sender As Object, e As EventArgs) Handles txtNumber.TextChanged
+        Me.lblResults.Text = Nothing
+    End Sub
+End Class
+
---
+++ oddNumberSum_tucker/oddNumberSum_tucker/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("oddNumberSum_tucker")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("oddNumberSum_tucker")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("82d2b6ba-9c86-47f3-a76a-510a8108e0f4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
